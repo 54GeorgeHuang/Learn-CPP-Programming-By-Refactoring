4d08359be286c2f103a3c59795e0b5b4840e9843
George Huang <54georgehuang@gmail.com>
2023-12-15 00:19:38 +0800

    ttc revised and commented

--- Tic_Tac_Toe/ttt_og.cpp
+++ Tic_Tac_Toe/ttt_og.cpp
@@ -2,63 +2,107 @@
 #include <cassert>
 
 #define SIZE 3
-#define PLAYERS 2
+#define N_PLAYERS 2
+#define PLAYER_A false
+#define PLAYER_B true
 
 int main(void)
 {
+	// init board, 1 = x, 0 = o, -1 = blank
 	int board[SIZE][SIZE];
-	for (int row = 0; row < SIZE; row++) {
-		for (int col = 0; col < SIZE; col++) {
+	for (int row = 0; row < SIZE; row++) 
+		for (int col = 0; col < SIZE; col++) 
 			board[row][col] = -1;
-		}
-	}
-	int rowSum[PLAYERS][SIZE];
-	int colSum[PLAYERS][SIZE];
-	int cross1[PLAYERS];
-	int cross2[PLAYERS];
-	for (int p = 0; p < PLAYERS; p++) {
+
+	// each player's activity on each line (used to check for winning)	
+	int rowSum[N_PLAYERS][SIZE];
+	int colSum[N_PLAYERS][SIZE];
+	int cross1[N_PLAYERS];
+	int cross2[N_PLAYERS];
+	for (int p = 0; p < N_PLAYERS; p++) {
 		cross1[p] = cross2[p] = 0;
-		for (int i = 0; i < SIZE; i++) {
+		for (int i = 0; i < SIZE; i++) 
 			rowSum[p][i] = colSum[p][i] = 0;
-		}
 	}
 
+	// total steps
 	int move {0};
 	bool win {false};
-	int player {0};
+	// whose turn
+	bool turn {PLAYER_A};
+
+	// main body
 	for (int i = 0; i < SIZE*SIZE; i++) {
 		int row, col;
-		std::cin >> row >> col;
-		assert( (row >= 0 && row < SIZE) && (col >= 0 && col < SIZE) );
-		assert(board[row][col] == -1);
-		board[row][col] = player;
-		rowSum[player][row]++;
-		colSum[player][col]++;
-		if (row == col) {
-			cross1[player]++;
-		}
-		if (row + col == SIZE-1) {
-			cross2[player]++;
-		}
-		for (int i = 0; i < SIZE; i++) {
-			if (rowSum[player][i] == SIZE || colSum[player][i] == SIZE) {
-				win = true;
+		bool input_ok;
+		do {
+			std::cout << "player " << turn << " turn: ";
+			std::cin >> row >> col;
+			input_ok = true;
+			// check for invalid input
+			if ( (row < 0 || row >= SIZE) || (col < 0 || col >= SIZE) ) {
+				input_ok = false;
+				std::cout << "invalid input" << std::endl;
+			} else if (board[row][col] != -1) {
+				input_ok = false;
+				std::cout << "taken" << std::endl;
 			}
-		}
-		if (cross1[player] == SIZE || cross2[player] == SIZE) {
-			win = true;
-		}
+		} while (input_ok == false);
+
+		// logging
+		board[row][col] = turn;
+		rowSum[turn][row]++;
+		colSum[turn][col]++;
+		if (row == col) 
+			cross1[turn]++;
+		if (row + col == SIZE-1) 
+			cross2[turn]++;
 		move++;
-		if (win) {
-			break;
+
+		// print the board
+		std::cout << std::endl;
+		for (int r = 0; r < SIZE; r++) 
+			for (int c = 0; c < SIZE; c++) {
+				switch (board[r][c]) {
+					case -1:
+						std::cout << "#";
+						break;
+					case 0:
+						std::cout << "0";
+						break;
+					case 1:
+						std::cout << "1";
+						break;
+				
+					default:
+						std::cerr << "something wrong with the board" << std::endl;
+						return 1;
+				}
+				if (c < SIZE-1) 
+					std::cout << " ";
+				else 
+					std::cout << std::endl;
+			}
+		std::cout << std::endl;
+
+		// check for winning
+		if (move >= 5) {
+			for (int i = 0; i < SIZE; i++) 
+				if (rowSum[turn][i] == SIZE || colSum[turn][i] == SIZE) 
+					win = true;
+			if (win != true && (cross1[turn] == SIZE || cross2[turn] == SIZE)) 
+				win = true;
+			if (win) 
+				break;
 		}
-		player = 1 - player;
+
+		turn = (turn == PLAYER_A)? PLAYER_B: PLAYER_A;
 	}
-	if (win) {
-		std::cout << "player " << player << " wins after " << move <<" moves." << std::endl;
-	} else {
+
+	if (win) 
+		std::cout << "player " << turn << " wins after " << move <<" moves." << std::endl;
+	else 
 		std::cout << "It is a tie." << std::endl;
-	}
 
 	return 0;
 }
